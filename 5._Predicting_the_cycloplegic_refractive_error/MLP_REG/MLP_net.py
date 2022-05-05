ad4628f4b33af6b16922d2ab3b6953b2f0c99254
ROOKLO <53211971+ROOKLO@users.noreply.github.com>
2022-05-05 09:46:04 +0800

    Update MLP_net.py

--- 5._Predicting_the_cycloplegic_refractive_error/MLP_REG/MLP_net.py
+++ 5._Predicting_the_cycloplegic_refractive_error/MLP_REG/MLP_net.py
@@ -9,43 +9,43 @@ class MLP_net_reg(torch.nn.Module):
         self.num_outputs = n_outputs
         self.dropout = torch.nn.Dropout(dropout)
 
-        self.hidden_0 = torch.nn.Linear(num_inputs, 1984)      # 30
+        self.hidden_0 = torch.nn.Linear(num_inputs, 1984)     
         self.bn0 = torch.nn.BatchNorm1d(1984)
 
-        self.hidden_1 = torch.nn.Linear(1984, 1408)           # 21
+        self.hidden_1 = torch.nn.Linear(1984, 1408)         
         self.bn1 = torch.nn.BatchNorm1d(1408)
 
-        self.hidden_2 = torch.nn.Linear(1408, 448)             # 6
+        self.hidden_2 = torch.nn.Linear(1408, 448)            
         self.bn2 = torch.nn.BatchNorm1d(448)
 
-        self.hidden_3 = torch.nn.Linear(448, 1856)             # 1856
+        self.hidden_3 = torch.nn.Linear(448, 1856)            
         self.bn3 = torch.nn.BatchNorm1d(1856)
 
-        self.hidden_4 = torch.nn.Linear(1856, 1280)            # 19
+        self.hidden_4 = torch.nn.Linear(1856, 1280)           
         self.bn4 = torch.nn.BatchNorm1d(1280)
 
-        self.hidden_5 = torch.nn.Linear(1280, 1344)             # 20
+        self.hidden_5 = torch.nn.Linear(1280, 1344)            
         self.bn5 = torch.nn.BatchNorm1d(1344)
 
         self.out = torch.nn.Linear(1344, n_outputs)
 
     def forward(self, x):
-        x = F.leaky_relu(self.hidden_0(x))  # activation function for hidden layer
+        x = F.leaky_relu(self.hidden_0(x))  
         x = self.dropout(self.bn0(x))
 
-        x = F.leaky_relu(self.hidden_1(x))  # activation function for hidden layer
+        x = F.leaky_relu(self.hidden_1(x))  
         x = self.dropout(self.bn1(x))
 
-        x = F.leaky_relu(self.hidden_2(x))  # activation function for hidden layer
+        x = F.leaky_relu(self.hidden_2(x))  
         x = self.dropout(self.bn2(x))
 
-        x = F.leaky_relu(self.hidden_3(x))  # activation function for hidden layer
+        x = F.leaky_relu(self.hidden_3(x)) 
         x = self.dropout(self.bn3(x))
 
-        x = F.leaky_relu(self.hidden_4(x))  # activation function for hidden layer
+        x = F.leaky_relu(self.hidden_4(x))  
         x = self.dropout(self.bn4(x))
 
-        x = F.leaky_relu(self.hidden_5(x))  # activation function for hidden layer
+        x = F.leaky_relu(self.hidden_5(x))  
         x = self.dropout(self.bn5(x))
         x = self.out(x)
         return x
